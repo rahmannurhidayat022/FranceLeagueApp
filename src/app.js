f5da169409b215fb06288f82a090aee0c1139a7f
rahmannurhidayat022 <rahmannurhidayat022@gmail.com>
2020-08-12 16:15:37 +0700

    memperbaiki PushManager agar bisa dijalan saat pertamakali clear cache

--- src/app.js
+++ src/app.js
@@ -57,23 +57,25 @@ const notification = () => {
 };
 
 const pushManager = () => {
-  if ("PushManager" in window) {
-    navigator.serviceWorker.getRegistration().then(function (registration) {
-      if (registration) {
-        registration.pushManager
-          .subscribe({
-            userVisibleOnly: true,
-            applicationServerKey: urlBase64(`${enums.PUBLIC_KEY}`),
-          })
-          .then((subscribe) => {
-            console.log("Berhasil melakukan subscribe dengan endpoint: ", subscribe.endpoint);
-            console.log("Berhasil melakukan subscribe dengan p256dh key: ", btoa(String.fromCharCode.apply(null, new Uint8Array(subscribe.getKey("p256dh")))));
-            console.log("Berhasil melakukan subscribe dengan auth key: ", btoa(String.fromCharCode.apply(null, new Uint8Array(subscribe.getKey("auth")))));
-          })
-          .catch((e) => {
-            console.error("Tidak dapat melakukan subscribe ", e.message);
-          });
-      }
-    });
-  }
+  navigator.serviceWorker.ready.then(() => {
+    if (('PushManager' in window)) {
+      navigator.serviceWorker.getRegistration().then(function (registration) {
+        if (registration) {
+          registration.pushManager
+            .subscribe({
+              userVisibleOnly: true,
+              applicationServerKey: urlBase64(`${enums.PUBLIC_KEY}`),
+            })
+            .then((subscribe) => {
+              console.log("Berhasil melakukan subscribe dengan endpoint: ", subscribe.endpoint);
+              console.log("Berhasil melakukan subscribe dengan p256dh key: ", btoa(String.fromCharCode.apply(null, new Uint8Array(subscribe.getKey("p256dh")))));
+              console.log("Berhasil melakukan subscribe dengan auth key: ", btoa(String.fromCharCode.apply(null, new Uint8Array(subscribe.getKey("auth")))));
+            })
+            .catch((e) => {
+              console.error("Tidak dapat melakukan subscribe ", e.message);
+            });
+        }
+      });
+    }
+  })
 };
